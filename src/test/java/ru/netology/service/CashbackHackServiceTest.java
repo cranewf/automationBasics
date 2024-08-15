913b18abe52ac5dd93cffeee859511397ac12f86
Vadim Zhuravlev <vadim.zhuravlev120498@mail.ru>
2024-08-15 13:54:59 +0300

    Reformat code

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class CashbackHackServiceTest {
     @Test
-    public void purchaseLessThan1000(){
+    public void purchaseLessThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
@@ -17,18 +17,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void purchaseMoreThan1000(){
+    public void purchaseMoreThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1200;
 
         int actual = service.remain(amount);
         int expected = 800;
 
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void purchaseEqualThan1000(){
+    public void purchaseEqualThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
