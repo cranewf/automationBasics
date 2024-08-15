402e4c328b7da12c717079b77866bdc103a58050
Vadim Zhuravlev <vadim.zhuravlev120498@mail.ru>
2024-08-15 13:31:55 +0300

    Update test in branch testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,39 @@
 package ru.netology.service;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
+    @Test
+    public void purchaseLessThan1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
 
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void purchaseMoreThan1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void purchaseEqualThan1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
 }
